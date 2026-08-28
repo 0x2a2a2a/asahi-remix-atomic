fcaa58197c78e349b8ae9c1b54da482f3857e6eb
Rance <272086955+0x2a2a2a@users.noreply.github.com>
2026-08-29 04:44:20 +0800

    a

--- build_files/02.asahi.sh
+++ build_files/02.asahi.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
+
+dnf -y install dnf5-plugins
+dnf -y copr enable @asahi/fedora-remix-branding
+dnf -y install asahi-repos
+
 dnf -y install \
   asahi-platform-metapackage-audio \
   asahi-platform-metapackage-desktop
