acd842a920df4f9222025f50cfa0ab121f0e3b95
Rance <272086955+0x2a2a2a@users.noreply.github.com>
2026-08-28 09:45:45 +0800

    chroot

--- build_files/selinux-copyup.sh
+++ build_files/selinux-copyup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
+# rootless mode
 # https://github.com/tuna-os/tunaOS/issues/1562
 # Force copy-up of /etc/selinux/targeted into the top layer so libsemanage's
 # atomic directory renames (tmp -> active) succeed on overlayfs.
